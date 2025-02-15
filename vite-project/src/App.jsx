2c130e7553843d471fab0fd7e37e418cf8428c7f
Rajalakshmi <you@example.com>
2025-02-15 23:25:33 +0530

    assignment-3

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -11,7 +11,13 @@ function App() {
     roll:18
   }
   alert("Hiiiiiii")
- console.log('My name is ',details.name)
+  console.log('My name is ',details.name)
+  function training(){
+    console.log("Welcome to react");
+  }
+  const fullstack=()=>{
+    console.log("Welcome to fullstack");
+  }
 
 
   return (
@@ -20,7 +26,11 @@ function App() {
       <h1 style={{textAlign:"center",color:"crimson"}}>RAJALAKSHMI</h1><br></br>
       <h2 style={{textAlign:'center'}}>External css</h2>
       <p className='info'>Name : {details.name}</p>
+      <button onclick={training}>Training</button>
+      <button onClick={fullstack}>Fullstack</button>
+      <button onClick={()=>console.log("student")}>Student</button>
     
     </>
   )
 }
+export default App
