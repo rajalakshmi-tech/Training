7ffca12eec6d81cf3dfc8bb862846a0b418f6e48
Rajalakshmi <you@example.com>
2025-02-19 08:58:50 +0530

    assignment 5

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,8 +2,14 @@
 import React from 'react'
 import './App.css'
 import Comp1 from './Comp1'
+import {useState} from 'react';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(0);
+  const changeTheme = () => {
+        setIsDarkMode((prevMode) => !prevMode);
+      };
+  
   const info={
     name:"Rajalakshmi",
     dept:"M.Tech cse",
@@ -17,23 +23,29 @@ function App() {
     age2:20,
   }
   
+  return (
+  <>
+    <div className={isDarkMode ? "dark-theme" : "light-theme"}>
+      <div className="content">
+        <h1 style={{ textAlign: "center", color: isDarkMode ? "#F9F6E6" : "darkred" }}>
+          Friends Forever 
+        </h1>
 
-return (
-
-    <>
-    <h1 style={{textAlign:"center",color:"black"}}>Friends Forever</h1>
-    <div>
-      <Comp1  name={info.name} age={info.age}/>
-      <Comp1 name2={info2.name2} age2={info2.age2}/>
+        <br />
+      <div>
+      <Comp1  name={info.name} age={info.age}  
+      name2={info2.name2} age2={info2.age2}/>
+      </div>
+      <h1 style={{textAlign:"center"}}>My SECID is {info.SecId}</h1>
+      <h1 style={{textAlign:"center"}}>My  friend's SECID is {info2.SecId}</h1>
+      <button onClick={()=>alert("On click is clicked")}>click</button><br></br>
+      <button onDoubleClick={()=>alert("On click is Double clicked")}>Double click</button> <br></br>
+      <button className="toggle-button" onClick={changeTheme}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}</button>
+      </div>
     </div>
-    <h1 style={{textAlign:"center",color:"black"}}>My SECID is {info.SecId}</h1>
-    <h1 style={{textAlign:"center",color:"black"}}>My  friend's SECID is {info2.SecId}</h1>
-    <button onClick={()=>alert("On click is clicked")}>click</button><br></br>
-    <button onDoubleClick={()=>alert("On click is Double clicked")}>Double click</button>
-  
-  
-    
-    </>
-  )
-}
-export default App
+
+  </>
+  );
+};
+export default App;
--- vite-project/src/Comp1.jsx
+++ vite-project/src/Comp1.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Comp1 = (props) => {
   return (
     <div>
-      <h3>{props.name}<br></br>{props.age}
-    {props.name2}<br></br>{props.age2}</h3>
+      <h3>
+        {props.name} {props.age}<br></br>
+        {props.name2} {props.age2}
+      </h3>
     
     </div>
     
